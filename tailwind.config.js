65922e4975d97e1cfdf004ac4068eaae0f78e50d
danghung <hungdang02042003@gmail.com>
2023-02-27 00:06:31 +0700

    initial

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,33 @@
 module.exports = {
-  darkMode: "class",
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        body: ["Epilogue", "sans-serif;"],
-      },
-    },
-  },
-  plugins: [],
+	darkMode: 'class',
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				body: ['Epilogue', 'sans-serif;'],
+			},
+			colors: {
+				primary: '#1dc071',
+				secondary: '#6f49fd',
+				text1: '#171725',
+				text2: '#4b5264',
+				text3: '#808191',
+				text4: '#b2b3bd',
+				'icon-color': '#a2a2a8',
+				white: '#fff',
+				whiteSoft: '#fcfbff',
+				graySoft: '#fcfcfc',
+				strock: '#f1f1f3',
+				lite: '#fcfcfd',
+				error: '#eb5757',
+				darkbg: '#13131a',
+				darkSecondary: '#1c1c24',
+				softDark: '#22222c',
+				darfSoft: '#24242c',
+				darkStroke: '#3a3a43',
+				darkRed: '#422c32',
+			},
+		},
+	},
+	plugins: [],
 };
